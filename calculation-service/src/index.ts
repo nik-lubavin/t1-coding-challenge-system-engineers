260db7aa2a95f5c3c21d369424da0096d7880594
Nikolai Liubavin <nik.lubavin@gmail.com>
2024-10-02 15:54:51 +0200

    calc - starting boilerplate added

--- calculation-service/src/index.ts
+++ calculation-service/src/index.ts
@@ -1,1 +1,36 @@
-// YOUR CODE HERE
+import Kafka from "node-rdkafka";
+
+import { TOPIC } from "./constants";
+
+// Create a consumer instance
+const consumer = new Kafka.KafkaConsumer(
+  {
+    "group.id": "my-group", // Consumer group id
+    "metadata.broker.list": "127.0.0.1:9092", // Kafka broker
+  },
+  {}
+);
+
+// Event when consumer is ready to consume messages
+consumer
+  .on("ready", () => {
+    console.log(`Consumer ready. Subscribing to topic ${TOPIC}...`);
+
+    // Subscribe to the topic
+    consumer.subscribe([TOPIC]);
+
+    // Start consuming messages
+    consumer.consume();
+  })
+  .on("data", (data) => {
+    // Message consumed from Kafka
+    console.log(`Consumed message: ${data?.value?.toString()}`);
+  });
+
+// Handle errors
+consumer.on("event.error", (err) => {
+  console.error("Error from consumer:", err);
+});
+
+// Connect to Kafka broker
+consumer.connect();
